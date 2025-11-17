d5c6d71aa77b57cf78ec0d817c980ef5f7c67342
Leticia Bi <leticiabi@LeticiadeMacBook-Air.local>
2025-11-17 16:51:24 +0100

    c++98

--- CPP01/ex01/zombieHorde.cpp
+++ CPP01/ex01/zombieHorde.cpp
@@ -2,12 +2,12 @@
 
 Zombie*	zombieHorde(int	N, std::string name) {
 	if (N <= 0)
-		return (nullptr);
+		return (NULL);
 	
 	Zombie* horde = new Zombie[N];
 
 	for (int i = 0; i < N; i++)
-		horde[i].setName(name + "[" + std::to_string(i) + "]");
+		horde[i].setName(name);
 
 	return (horde);
 }
